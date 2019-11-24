7b104ca1889e226b42f88221bb1fc79847ca6739
zhao-x <zhao-x@techocean.co.jp>
2019-11-24 13:13:18 +0900

    add operator

--- src/Rebib/Database/QueryBuilder/Builder/GroupQueryBuilder.php
+++ src/Rebib/Database/QueryBuilder/Builder/GroupQueryBuilder.php
@@ -10,8 +10,41 @@ class GroupQueryBuilder extends Builder
      */
     private $groups = [];
 
+    /**
+     *
+     * @param array $groups
+     * @param bool $force
+     * @return GroupQueryBuilder
+     */
+    public function setFields(array $groups, bool $force = false): GroupQueryBuilder
+    {
+        if ($force === true) {
+            $this->groups = $groups;
+        } else {
+            foreach ($groups as $group) {
+                $this->addGroup($group);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $group
+     * @return GroupQueryBuilder
+     */
+    public function addGroup(string $group): GroupQueryBuilder
+    {
+        if (!in_array($group, $this->groups)) {
+            $this->groups[] = $group;
+        }
+        return $this;
+    }
+
     public function buildQuery(): string
     {
-        return $this->arrayToString($this->groups, ',');
+        if (!$this->groups) {
+            return '';
+        }
+        return 'GROUP BY '.$this->arrayToString($this->groups, ',');
     }
 }
