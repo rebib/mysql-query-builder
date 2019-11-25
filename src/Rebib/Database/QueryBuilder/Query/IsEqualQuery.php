39d79df1dba6db51f198aa526cd809a872ac830c
zhao-x <zhao-x@techocean.co.jp>
2019-11-25 23:23:01 +0900

    add query creator

--- src/Rebib/Database/QueryBuilder/Query/IsEqualQuery.php
+++ src/Rebib/Database/QueryBuilder/Query/IsEqualQuery.php
@@ -17,7 +17,7 @@ class IsEqualQuery extends EqualQuery
      * @param bool $bind bind parameter or not
      * @return EqualQuery
      */
-    public function add(string $expr, string $value, bool $bind = true): IsEqualQuery
+    public function add(string $expr, string $value, bool $bind = true): EqualQuery
     {
         if (in_array(strtoupper($value), ['TRUE', 'FALSE', 'UNKNOWN', 'NULL'])) {            
             return $this;
