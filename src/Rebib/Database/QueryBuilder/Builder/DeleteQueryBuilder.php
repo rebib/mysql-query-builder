bde8d8178302557c18180f16dcc76f7d65253a6b
zhao-x <zhao-x@techocean.co.jp>
2019-11-25 21:59:43 +0900

    add query creator

--- src/Rebib/Database/QueryBuilder/Builder/DeleteQueryBuilder.php
+++ src/Rebib/Database/QueryBuilder/Builder/DeleteQueryBuilder.php
@@ -5,6 +5,20 @@ namespace Rebib\Database\QueryBuilder\Builder;
 class DeleteQueryBuilder extends CRUDQueryBuilder
 {
 
+    /**
+     * Constructor
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __clone()
+    {
+
+    }
+
     public function buildQuery(): string
     {
         //TODO
--- src/Rebib/Database/QueryBuilder/Builder/UpdateQueryBuilder.php
+++ src/Rebib/Database/QueryBuilder/Builder/UpdateQueryBuilder.php
@@ -5,6 +5,20 @@ namespace Rebib\Database\QueryBuilder\Builder;
 class UpdateQueryBuilder extends CRUDQueryBuilder
 {
 
+    /**
+     * Constructor
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __clone()
+    {
+
+    }
+
     public function buildQuery(): string
     {
         //TODO
